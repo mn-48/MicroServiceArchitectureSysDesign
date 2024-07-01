a387b48056c15ebca5d16944797ff9b35e562759
mn-48 <nazmul.cse48@gmail.com>
2024-07-01 14:30:40 +0600

    djangosession middleware

--- dj_mysql_docker/backend/core/settings.py
+++ dj_mysql_docker/backend/core/settings.py
@@ -57,12 +57,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", # new
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware", # new
+    
 ]
 
 ROOT_URLCONF = "core.urls"
