1b57c7ae3126e50825200f3b88314a95e7ce860e
mn-48 <nazmul.cse48@gmail.com>
2024-07-01 14:43:42 +0600

    user authentication

--- dj_mysql_docker/backend/users/serializers.py
+++ dj_mysql_docker/backend/users/serializers.py
@@ -5,9 +5,20 @@ from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
