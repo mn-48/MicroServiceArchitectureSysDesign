5839793e94cb60ae1ca021e164cf18445a28f8c0
mn-48 <nazmul.cse48@gmail.com>
2024-07-07 12:27:24 +0600

    modified:   dj_mysql_docker/backend/.mysql_data/#ib_16384_0.dblwr         modified:   dj_mysql_docker/backend/.mysql_data/#innodb_redo/#ib_redo10         modified:   dj_mysql_docker/backend/.mysql_data/binlog.000018         modified:   dj_mysql_docker/backend/.mysql_data/ibdata1         modified:   dj_mysql_docker/backend/.mysql_data/mydatabase/products_product.ibd         modified:   dj_mysql_docker/backend/.mysql_data/mysql.ibd         modified:   dj_mysql_docker/backend/.mysql_data/undo_001         modified:   dj_mysql_docker/backend/products/serializers.py         modified:   dj_mysql_docker/backend/products/urls.py

--- dj_mysql_docker/backend/products/serializers.py
+++ dj_mysql_docker/backend/products/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
+    # image_url = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields = '__all__'
--- dj_mysql_docker/backend/products/urls.py
+++ dj_mysql_docker/backend/products/urls.py
@@ -1,12 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet
+from rest_framework.routers import DefaultRouter # type: ignore
 
-# Create a router and register the viewset with it
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='products')
-
-# Define URL patterns
-urlpatterns = [
-    path('', include(router.urls)),  # Includes all the URLs from the router
-]
+urlpatterns = router.urls
